e94dafb3cf6c7f62f06544da385e807c32e0a35f
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-07-25 00:17:50 +0200

    feat: it creates default note when visiting index page.

--- notes/documents/__init__.py
+++ notes/documents/__init__.py
@@ -2,7 +2,14 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 bp = Blueprint("documents", __name__)
 
+from . import service
+from . import repository
+
 
 @bp.route("/")
 def index():
-    return render_template("documents/index.html")
+    store = repository.InMemoryDocumentRepository()
+    create_document = service.CreateDocument(store)
+    document = create_document.execute(title="Untitled")
+
+    return render_template("documents/index.html", title=document.title)
